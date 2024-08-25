b712e85d6782ec132be53c079e587faf742e306f
mustafa ansari <mustafaansari@mail.com>
2024-08-25 10:10:08 +0530

    some changes

--- Graphs/Medium/BFS_Traversal_of_Graph.py
+++ Graphs/Medium/BFS_Traversal_of_Graph.py
@@ -1,22 +1,22 @@
 # Problem URL: https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+
 from typing import List
 from queue import Queue
 class Solution:
     #Function to return Breadth First Traversal of given graph.
     def bfsOfGraph(self, V: int, adj: List[List[int]]) -> List[int]:
             # code here
+            res=[]
+            Q=Queue()
+            Q.put(0)
             visited=set()
-            queue=[0]
-            result=[]
-            while queue:
-                node=queue.pop(0)
+            while not Q.empty():
+                node=Q.get()
                 if node not in visited:
                     visited.add(node)
-                    result.append(node)
-                for neighbor in adj[node]:
-                    if neighbor not in visited:
-                        queue.append(neighbor)
-            return result
+                    res.append(node)
+                for neighbour in adj[node]:
+                    if neighbour not in visited:
+                        Q.put(neighbour)
+            return res
             
-
-
--- Graphs/Medium/DFS_for_a_Graph.py
+++ Graphs/Medium/DFS_for_a_Graph.py
@@ -1,26 +1,31 @@
 # Problem URL: https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
 class Solution:
-        def depth_first_search(self,node,adj,visited=None):
-            if visited is None:
-                 visited=set()
-            result = []
-            visited.add(node)
-            result.append(node)
-            for neighbor in adj[node]:
-                if neighbor not in visited:
-                    result.extend(self.depth_first_search(neighbor,adj,visited))
-            return result
-        
     
-
-if __name__=="__main__":
-    graph={
-        "5":["3","7"],
-        "3":["2","4"],
-        "2":[],
-        "4":["7"],
-        "8":[],
-        "7":["8"]
-    }
-    result=Solution().depth_first_search("5",graph)
-    print(result)
+    #Function to return a list containing the DFS traversal of the graph.
+    def dfsOfGraph(self, V, adj):
+        res=[]
+        visited=set()
+        stack=[0]
+        while stack:
+            node=stack.pop()
+            if node not in visited:
+                visited.add(node)
+                res.append(node)
+                for  nodes in reversed(adj[node]):
+                    if nodes not in visited:
+                        stack.append(nodes)
+        return res
+    
+        def dfsrecurssive(node, visited, res):
+            visited.add(node)
+            res.append(node)
+            for nodes in adj[node]:
+                if nodes not in visited:
+                    visited.add(nodes)
+                    dfsrecurssive(nodes, visited, res)
+            return res
+            
+        res=[]
+        visited=set()
+        return dfsrecurssive(0, visited, res)
+        
